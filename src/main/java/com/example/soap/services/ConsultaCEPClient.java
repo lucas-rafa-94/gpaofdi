34e55e05928e38279fc6726011995474955b47cb
lucas dos santos <lucas_rafa_94@hotmail.com>
2018-03-27 18:17:17 -0300

    t

--- src/main/java/com/example/soap/services/ConsultaCEPClient.java
+++ src/main/java/com/example/soap/services/ConsultaCEPClient.java
@@ -11,16 +11,14 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBIntrospector;
 import javax.xml.namespace.QName;
 
-
 public class ConsultaCEPClient extends WebServiceGatewaySupport {
 
     public ConsultaCEPResponse getCep(String cep) {
         ConsultaCEP request = new ConsultaCEP();
         request.setCep(cep);
-
         ConsultaCEPResponse response = (ConsultaCEPResponse) JAXBIntrospector.getValue(getWebServiceTemplate()
                 .marshalSendAndReceive(
-                        "https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente",
+                        "https://apps.correios.com.br/gragra/AtendeClienteService/AtendeCliente",
                         new JAXBElement<ConsultaCEP>(new QName("http://cliente.bean.master.sigep.bsb.correios.com.br/", "consultaCEP", "cli"), ConsultaCEP.class, request)));
 
         return response;
